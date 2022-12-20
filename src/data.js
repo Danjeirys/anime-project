58118b5fc9e6fdfbbaf38db56a3955faf98bb07a
Jerry Martinez <jaysavageg@gmail.com>
2022-12-20 12:50:10 -0500

    local database

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default data = [
+    {
+        title: 'The Rise of The Shield Hero', 
+        poster: 'https://flxt.tmsimg.com/assets/p16918973_b_v9_aa.jpg', 
+        genre: 'isekai',
+        about: 'A gamer is magically summoned into a parallel universe, where he is chosen as one of four heroes destined to save the world from its prophesied doom. Stories of old tell of four otherworldly heroes - wielding the sword, spear, bow, and shield - who defended the land from wave after wave of calamity.',
+        releaseDate: 'January 9, 2019', 
+    }, 
+    {
+        title: 'OverLord',
+        poster: 'https://cdn.shopify.com/s/files/1/0747/3829/products/mL2238_18782a2d-c1ad-414e-9624-d38d2550cca7_1024x1024.jpg?v=1571445521',
+        genre: 'isekai',
+        about: 'An office worker in a dystopian world logs onto a video game for the last time only to find out that he, along with his entire guild, has been transported to another reality.',
+        releaseDate: 'September 30, 2016'
+    }, 
+    {
+        title: 'Goblin Slayer', 
+        poster: 'https://pbs.twimg.com/media/FOw8j8HVQAsyPIU.jpg', 
+        genre: 'isekai', 
+        about: 'In a fantasy world, a lone hero makes his living by exterminating all goblins he encounters. But one day he meets a friend, and his life begins to become more intense.', 
+        releaseDate: 'October 6, 2018'
+    }
+]
